a1e6e4885d670481e2fb58d3924fd9cf07227473
Silk_Crab <r.s.ricce@gmail.com>
2023-08-13 12:03:31 +0200

    Функция заупска ботов после перезагрузки.

--- traider_bot/gunicorn.conf.py
+++ traider_bot/gunicorn.conf.py
@@ -5,9 +5,9 @@ import pickle
 import django
 
 from bots.bb_set_takes import set_takes
-from bots.bot_logic import create_bb_and_avg_obj
+from bots.bot_logic import create_bb_and_avg_obj, clear_data_bot
 from bots.hedge.logic.work import set_takes_for_hedge_grid_bot
-from bots.terminate_bot_logic import terminate_thread
+from bots.terminate_bot_logic import terminate_thread, check_thread_alive, stop_bot_with_cancel_orders
 from single_bot.logic.global_variables import *
 from single_bot.logic.work import bot_work_logic
 
@@ -17,78 +17,39 @@ django.setup()
 from bots.models import Bot, IsTSStart
 
 
-# def when_ready(server):
-#     time.sleep(10)
-#     try:
-#         with open("bot_id.pkl", "rb") as file:
-#             bot_id_list = pickle.load(file)
-#         if bot_id_list:
-#             server.log.info(bot_id_list)
-#             for bot_id in bot_id_list:
-#                 bot = Bot.objects.filter(pk=bot_id).first()
-#                 if bot:
-#                     bot_thread = None
-#                     is_ts_start = IsTSStart.objects.filter(bot=bot)
-#                     if bot.work_model == 'bb':
-#                         if bot.side == 'TS':
-#                             if is_ts_start:
-#                                 bot_thread = threading.Thread(target=set_takes, args=(bot,))
-#                             else:
-#                                 bot_thread = threading.Thread(target=set_takes_for_hedge_grid_bot, args=(bot,))
-#                         else:
-#                             bot_thread = threading.Thread(target=set_takes, args=(bot,))
-#                     elif bot.work_model == 'grid':
-#                         if bot.side == 'TS':
-#                             if is_ts_start:
-#                                 bot_thread = threading.Thread(target=bot_work_logic, args=(bot,))
-#                             else:
-#                                 bot_thread = threading.Thread(target=set_takes_for_hedge_grid_bot, args=(bot,))
-#                         else:
-#                             bot_thread = threading.Thread(target=bot_work_logic, args=(bot,))
-#                     if bot_thread is not None:
-#                         bot_thread.start()
-#                         lock.acquire()
-#                         global_list_threads[bot_id] = bot_thread
-#                         if lock.locked():
-#                             lock.release()
-#     except Exception as e:
-#         server.log.info(f"Ошибка запуска потока - {e}")
-#
-#     finally:
-#         server.log.info("Gunicorn готов принимать запросы.")
-#
-#
-# def worker_exit(server, worker):
-#     try:
-#         os.remove("bot_id.pkl")
-#         print("Файл успешно удален.")
-#     except FileNotFoundError:
-#         print("Файл не найден.")
-#     except PermissionError:
-#         print("Отсутствуют права доступа для удаления файла.")
-#     except Exception as e:
-#         print(f"Ошибка при удалении файла: {e}")
-#     lock.acquire()
-#     try:
-#         if global_list_threads:
-#             with open("bot_id.pkl", "wb") as file:
-#                 pickle.dump(list(global_list_threads.keys()), file)
-#
-#         bots_id_list = global_list_bot_id
-#         for bot_id in bots_id_list:
-#             global_list_bot_id.remove(bot_id)
-#             thread = global_list_threads[bot_id]
-#             if lock.locked():
-#                 lock.release()
-#             thread.join()
-#             lock.acquire()
-#             del global_list_threads[bot_id]
-#             print(f"Удален из списка бот {bot_id}")
-#
-#     finally:
-#         if lock.locked():
-#             lock.release()
-#         server.log.info(f"Worker {worker.pid} остановлен.")
+def bot_start_reboot(bot_id):
+    bot = Bot.objects.get(pk=bot_id)
+    bot_thread = None
+    is_ts_start = IsTSStart.objects.filter(bot=bot)
+
+    if check_thread_alive(bot.pk):
+        stop_bot_with_cancel_orders(bot)
+
+    clear_data_bot(bot)  # Очищаем данные ордеров и тейков которые использовал старый бот
+
+    if bot.work_model == 'bb':
+        if bot.side == 'TS':
+            if is_ts_start:
+                bot_thread = threading.Thread(target=set_takes, args=(bot,))
+            else:
+                bot_thread = threading.Thread(target=set_takes_for_hedge_grid_bot, args=(bot,))
+        else:
+            bot_thread = threading.Thread(target=set_takes, args=(bot,))
+    elif bot.work_model == 'grid':
+        if bot.side == 'TS':
+            if is_ts_start:
+                bot_thread = threading.Thread(target=bot_work_logic, args=(bot,))
+            else:
+                bot_thread = threading.Thread(target=set_takes_for_hedge_grid_bot, args=(bot,))
+        else:
+            bot_thread = threading.Thread(target=bot_work_logic, args=(bot,))
+
+    if bot_thread is not None:
+        bot_thread.start()
+        lock.acquire()
+        global_list_threads[bot.pk] = bot_thread
+        if lock.locked():
+            lock.release()
 
 
 def worker_exit(server, worker):
@@ -98,6 +59,17 @@ def worker_exit(server, worker):
 
 
 def when_ready(server):
-    all_bots_pks = Bot.objects.values_list('pk', flat=True).order_by('pk')
-    for bot_id in all_bots_pks:
-        print(terminate_thread(bot_id))
+    try:
+        all_bots_pks = Bot.objects.values_list('pk', flat=True).order_by('pk')
+        for bot_id in all_bots_pks:
+            print(terminate_thread(bot_id))
+
+        time.sleep(5)
+
+        for bot_id in all_bots_pks:
+            bot_start_reboot(bot_id=bot_id)
+
+        print('Запуск произведен')
+
+    except Exception as e:
+        print(f'При старте воркера возникла ошибка {e}')
