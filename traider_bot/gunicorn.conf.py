9b1c798aee3c1d44e2f8b7d74476dcf20527a12b
Silk_Crab <r.s.ricce@gmail.com>
2023-11-16 18:54:18 +0100

    закомментирование содержимого gunicorn.config.py

--- traider_bot/gunicorn.conf.py
+++ traider_bot/gunicorn.conf.py
@@ -1,20 +1,20 @@
-import os
-
-import django
-
-from bots.terminate_bot_logic import terminate_thread
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'traider_bot.settings')
-django.setup()
-
-from bots.models import Bot
-
-
-def worker_exit(server, worker):
-    all_bots_pks = Bot.objects.values_list('pk', flat=True).order_by('pk')
-    for bot_id in all_bots_pks:
-        print(terminate_thread(bot_id, keep_active=True))
-
-
-def when_ready(server):
-    pass
+# import os
+#
+# import django
+#
+# from bots.terminate_bot_logic import terminate_thread
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'traider_bot.settings')
+# django.setup()
+#
+# from bots.models import Bot
+#
+#
+# def worker_exit(server, worker):
+#     all_bots_pks = Bot.objects.values_list('pk', flat=True).order_by('pk')
+#     for bot_id in all_bots_pks:
+#         print(terminate_thread(bot_id, keep_active=True))
+#
+#
+# def when_ready(server):
+#     pass
