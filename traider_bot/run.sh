5ed84d88cce39b63f80da492b00b99295cbd8ece
Silk_Crab <r.s.ricce@gmail.com>
2024-03-18 19:47:41 +0100

    update start with gunicorn.wsgi & nginx

--- traider_bot/run.sh
+++ traider_bot/run.sh
@@ -7,4 +7,7 @@ python manage.py migrate
 python ./traider_bot/start_ws_manager.py
 
 # Запускаем сервер Django
-python manage.py runserver 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
+
+# Запускаем сервер gunicorn
+gunicorn traider_bot.wsgi:application --bind 0.0.0.0:8000
--- traider_bot/traider_bot/start_ws_manager.py
+++ traider_bot/traider_bot/start_ws_manager.py
@@ -4,10 +4,12 @@ import requests
 
 
 def start_ws_manager():
-    url = "http://localhost:8008/ws/manager/start"
-    requests.get(url)
+    url = "http://ws-manager:8008/ws/manager/start"
+    print(url)
+    print(requests.get(url))
+    print(url)
 
 
 if __name__ == '__main__':
-    time.sleep(10)
+    time.sleep(3)
     start_ws_manager()
