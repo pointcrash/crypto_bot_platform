44b6998518a4d12dcd5a0b000fcb129fc80fb4bd
Silk_Crab <r.s.ricce@gmail.com>
2024-03-28 19:09:30 +0100

    delete one_way, hedge_mode and grid_mode directory

--- traider_bot/bots/hedge/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from bots.hedge.views_grid import hedge_grid_bots_list, hedge_grid_create_bot, hedge_grid_bot_detail
-
-urlpatterns = [
-    path('grid/list', hedge_grid_bots_list, name='hedge_grid_list'),
-    path('grid/create', hedge_grid_create_bot, name='hedge_grid_create'),
-    path('grid/detail/<int:bot_id>', hedge_grid_bot_detail, name='hedge_grid_detail'),
-]
