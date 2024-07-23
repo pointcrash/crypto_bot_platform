bb5583c4dab48e065d8eda9acfa0bd452c40b957
Natalia <natalia.vayngolts@gmail.com>
2024-07-23 12:00:48 +0300

    Execute migration for the python app before killing the main process

--- ansible/restart_main/files/restart-python.sh
+++ ansible/restart_main/files/restart-python.sh
@@ -10,5 +10,5 @@ repo_dest=$1
 # Read main Python process PID
 main_process_pid=$(cat "$repo_dest/bot-python/traider_bot/gunicorn.pid")
 
-# Kill the main Python process
-docker exec main-1 kill -HUP $main_process_pid
+# Run migrate script and kill the main Python process
+docker exec main-1 python manage.py migrate && kill -HUP $main_process_pid
