2264a4d98a5ead4e57b257e1f60c7cd2cf93e95b
pointcrash <r.s.ricce@gmail.com>
2024-07-26 13:24:45 +0200

    add to admin doc and sup models

--- traider_bot/documentation/admin.py
+++ traider_bot/documentation/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from documentation.models import DocPage, DocTag
+
+
+@admin.register(DocPage)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'body', 'created_at', 'updated_at')
+    list_display_links = ('id', 'title',)
+    search_fields = ('id', 'title', 'created_at')
+
+
+@admin.register(DocTag)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'doc_page', 'created_at')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'doc_page', 'created_at')
--- traider_bot/support/admin.py
+++ traider_bot/support/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from support.models import SupportTicket, TicketMessage
+
+
+@admin.register(SupportTicket)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner', 'stuff', 'status', 'created_at', 'updated_at')
+    list_display_links = ('id', 'title',)
+    search_fields = ('id', 'title', 'owner', 'stuff', 'status', 'created_at')
+
+
+@admin.register(TicketMessage)
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'ticket', 'is_read', 'created_at')
+    list_display_links = ('id', )
+    search_fields = ('id', 'author', 'ticket', 'is_read', 'created_at')
