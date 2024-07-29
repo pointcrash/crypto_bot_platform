c1e821cdf67dae4f8f36e6d344e0e69bfc8def89
Natalia <natalia.vayngolts@gmail.com>
2024-07-29 12:10:12 +0300

    Echo stages of restart-python.sh

--- ansible/restart_main/files/restart-python.sh
+++ ansible/restart_main/files/restart-python.sh
@@ -11,5 +11,7 @@ repo_dest=$1
 main_process_pid=$(cat "$repo_dest/bot-python/traider_bot/gunicorn.pid")
 
 # Run migrate script and kill the main Python process
+echo "Start migration"
 docker exec main-1 python manage.py migrate
+echo "Killing main process to restart"
 docker exec main-1 sleep 10 && kill -HUP $main_process_pid
