589341a774ce2b4512f2eb348bd995504f80ed66
pointcrash <r.s.ricce@gmail.com>
2024-08-21 12:58:01 +0200

    change lifetime jwt tokens

--- traider_bot/traider_bot/settings.py
+++ traider_bot/traider_bot/settings.py
@@ -244,8 +244,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
 }
 
 SESSION_COOKIE_SECURE = True
