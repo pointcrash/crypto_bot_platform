407034186305d28f0e896bf7be63f75e6cbfd55d
pointcrash <r.s.ricce@gmail.com>
2024-10-05 13:33:55 +0200

    \

--- traider_bot/tg_bot/send_message.py
+++ traider_bot/tg_bot/send_message.py
@@ -1,5 +1,5 @@
 import requests
-from .start_app import API_TOKEN
+from .connect_tg_bot import bot_token
 
 
 def send_telegram_message(chat_id, bot=None,  message='None'):
@@ -9,7 +9,7 @@ def send_telegram_message(chat_id, bot=None,  message='None'):
         message = account_info + bot_info + message
 
     c = 0
-    url = f"https://api.telegram.org/bot{API_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     data = {"chat_id": chat_id, "text": message}
     while c < 10:
         c += 1
