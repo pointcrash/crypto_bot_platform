2f971aa3c96c9b8bf53d7a0f4557d42cbe6fc8e3
pointcrash <r.s.ricce@gmail.com>
2024-10-09 17:17:52 +0200

    .

--- traider_bot/purchases/urls_api.py
+++ traider_bot/purchases/urls_api.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from purchases.vews_api import ServiceProductViewSet, PurchaseViewSet, create_invoice_view, purchases_callback_view
+from purchases.vews_api import ServiceProductViewSet, PurchaseViewSet, CreateInvoiceView, PurchasesCallbackView
 
 router = DefaultRouter()
 router.register(r'products', ServiceProductViewSet)
@@ -9,6 +9,6 @@ router.register(r'purchases', PurchaseViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('create_invoice/', create_invoice_view, name='create_invoice'),
-    path('purchases/callback/', purchases_callback_view, name='purchases_callback'),
+    path('purchases/create_invoice/', CreateInvoiceView.as_view()),
+    path('purchases/callback/', PurchasesCallbackView.as_view()),
 ]
--- traider_bot/purchases/vews_api.py
+++ traider_bot/purchases/vews_api.py
@@ -4,7 +4,9 @@ import requests
 from django.conf import settings
 from django.http import JsonResponse
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.views import APIView
+
 from .models import ServiceProduct, Purchase
 from .serializers import ServiceProductSerializer, PurchaseSerializer
 
@@ -32,62 +34,65 @@ class PurchaseViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-def create_invoice_view(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({'success': False, 'message': 'Пользователь не авторизован'}, status=401)
+class CreateInvoiceView(APIView):
+    permission_classes = [IsAuthenticated, ]
 
-    if request.method == 'POST':
-        url = 'https://api.cryptocloud.plus/v2/invoice/create'
-        auth_token = settings.CRYPTOCLOUD_AUTH_TOKEN
-        shop_id = settings.CRYPTOCLOUD_SHOP_ID
-        order_id = uuid.uuid4().hex
-        amount = request.POST.get('amount')
-        email = request.POST.get('email')
+    def post(self, request):
+        if request.method == 'POST':
+            url = 'https://api.cryptocloud.plus/v2/invoice/create'
+            auth_token = settings.CRYPTOCLOUD_AUTH_TOKEN
+            shop_id = settings.CRYPTOCLOUD_SHOP_ID
+            order_id = uuid.uuid4().hex
+            amount = request.POST.get('amount')
+            email = request.POST.get('email')
 
-        if not amount or not email:
-            return JsonResponse({'success': False, 'message': 'Не переданы обязательные параметры'}, status=400)
+            if not amount or not email:
+                return JsonResponse({'success': False, 'message': 'Не переданы обязательные параметры'}, status=400)
 
-        data = {
-            'shop_id': shop_id,
-            'amount': amount,
-            'currency': 'USD',
-            'email': email,
-            'order_id': order_id,
-        }
+            data = {
+                'shop_id': shop_id,
+                'amount': amount,
+                'currency': 'USD',
+                'email': email,
+                'order_id': order_id,
+            }
 
-        headers = {
-            'Authorization': f'Token {auth_token}',
-            'Content-Type': 'application/json'
-        }
+            headers = {
+                'Authorization': f'Token {auth_token}',
+                'Content-Type': 'application/json'
+            }
 
-        try:
-            response = requests.post(url, json=data, headers=headers)
-            response_data = response.json()
+            try:
+                response = requests.post(url, json=data, headers=headers)
+                response_data = response.json()
 
-            if response.status_code == 200 and 'result' in response_data:
-                return JsonResponse({'success': True, 'link': response_data['result']['link']})
-            else:
-                return JsonResponse(
-                    {'success': False, 'message': response_data.get('message', 'Ошибка при создании счета')},
-                    status=response.status_code)
+                if response.status_code == 200 and 'result' in response_data:
+                    return JsonResponse({'success': True, 'link': response_data['result']['link']})
+                else:
+                    return JsonResponse(
+                        {'success': False, 'message': response_data.get('message', 'Ошибка при создании счета')},
+                        status=response.status_code)
 
-        except Exception as e:
-            return JsonResponse({'success': False, 'message': 'Некорректный ответ от API', 'error': str(e)}, status=500)
+            except Exception as e:
+                return JsonResponse({'success': False, 'message': 'Некорректный ответ от API', 'error': str(e)}, status=500)
 
-    else:
-        return JsonResponse({'success': False, 'message': 'Неверный тип запроса (должен быть POST)'}, status=500)
+        else:
+            return JsonResponse({'success': False, 'message': 'Неверный тип запроса (должен быть POST)'}, status=500)
 
 
-def purchases_callback_view(request):
-    if request.method == 'POST':
-        status = request.POST.get('status')
-        invoice_id = request.POST.get('invoice_id')
-        amount_crypto = request.POST.get('amount_crypto')
-        currency = request.POST.get('currency')
-        order_id = request.POST.get('order_id')
-        token = request.POST.get('token')
+class PurchasesCallbackView(APIView):
+    permission_classes = [AllowAny]
 
-        print(order_id)
-        print(status)
-        print(invoice_id)
+    def post(self, request):
+        if request.method == 'POST':
+            status = request.POST.get('status')
+            invoice_id = request.POST.get('invoice_id')
+            amount_crypto = request.POST.get('amount_crypto')
+            currency = request.POST.get('currency')
+            order_id = request.POST.get('order_id')
+            token = request.POST.get('token')
+
+            print(order_id)
+            print(status)
+            print(invoice_id)
 
