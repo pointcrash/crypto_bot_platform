767a9f74c5be74745c363c768f388920c05ebebf
pointcrash <r.s.ricce@gmail.com>
2024-10-13 20:46:09 +0200

    /

--- traider_bot/tariffs/models.py
+++ traider_bot/tariffs/models.py
@@ -3,12 +3,21 @@ from django.db import models
 
 
 class Tariff(models.Model):
+    TARIFF_TYPE_CHOICES = (
+        ('ACTIVE', 'ACTIVE'),
+        ('INACTIVE', 'INACTIVE'),
+        ('ARCHIVED', 'ARCHIVED'),
+        ('ADMIN', 'ADMIN'),
+    )
+
     title = models.CharField(max_length=50)
     max_accounts = models.IntegerField()
     max_bots = models.IntegerField()
     max_income_per_month = models.IntegerField()
     response_time_from_support = models.IntegerField(default=7)
     price = models.DecimalField(max_digits=5, decimal_places=2, default=10)
+    type = models.CharField(choices=TARIFF_TYPE_CHOICES, default='inactive', null=True)
+
 
     def __str__(self):
         return self.title
