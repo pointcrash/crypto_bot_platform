a056d99a88c011dd894ef5ec6d76bd08fd34757d
pointcrash <r.s.ricce@gmail.com>
2024-11-14 02:08:35 +0100

    int celery

--- traider_bot/traider_bot/celery_app.py
+++ traider_bot/traider_bot/celery_app.py
@@ -1,8 +1,10 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'traider_bot.settings')
 
 app = Celery('traider_bot')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
