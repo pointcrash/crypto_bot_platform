06d4c8a7b0d1b7ee69d7bcf60dc7fa2042b34166
pointcrash <r.s.ricce@gmail.com>
2024-12-05 15:12:54 +0100

    add language-fields for doctype models

--- traider_bot/documentation/models.py
+++ traider_bot/documentation/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+LANGUAGE_CHOICE = (
+    ('en', 'en'),
+    ('ru', 'ru'),
+)
+
 
 class DocPage(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField(blank=True)
+    language = models.CharField(max_length=5, choices=LANGUAGE_CHOICE, default='en')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -15,9 +21,11 @@ class DocPage(models.Model):
 class DocTag(models.Model):
     title = models.CharField(max_length=50, unique=True)
     doc_page = models.ManyToManyField(DocPage, related_name='tags', blank=True)
+    language = models.CharField(max_length=5, choices=LANGUAGE_CHOICE, default='en')
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class DocCategory(models.Model):
     title = models.CharField(max_length=200)
     doc_page = models.ManyToManyField(DocPage, related_name='category')
+    language = models.CharField(max_length=5, choices=LANGUAGE_CHOICE, default='en')
