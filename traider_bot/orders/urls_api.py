5b21a2911d9816d13b7980c259455fe3c1074519
pointcrash <r.s.ricce@gmail.com>
2024-12-17 19:17:54 +0100

    add view and url for cancel order

--- traider_bot/orders/urls_api.py
+++ traider_bot/orders/urls_api.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from orders.views_api import PositionViewSet
+from orders.views_api import PositionViewSet, CancelOrderView
 
 urlpatterns = [
     path('positions/', PositionViewSet.as_view({'get': 'list_by_bots'})),
+    path('orders/cancel-order/<int:order_pk>/', CancelOrderView.as_view()),
 
 ]
--- traider_bot/orders/views_api.py
+++ traider_bot/orders/views_api.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from api_2.api_aggregator import cancel_order
 from bots.models import BotModel
-from orders.models import Position
+from orders.models import Position, Order
 from orders.serializers import PositionSerializer
+from traider_bot.permissions import IsOrderOwnerOrAdmin
 
 
 class PositionViewSet(viewsets.ModelViewSet):
@@ -30,3 +33,15 @@ class PositionViewSet(viewsets.ModelViewSet):
 
 def login_test(request):
     return render(request, 'login_test.html')
+
+
+class CancelOrderView(APIView):
+    permission_classes = [IsOrderOwnerOrAdmin]
+
+    def post(self, request, order_pk):
+        try:
+            order = Order.objects.get(pk=order_pk)
+            cancel_order(order.bot, order.order_id)
+            return Response({"detail": "Order cancelled"}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"detail": f"Get error {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
