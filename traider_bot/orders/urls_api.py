7bd26f83d7cf84ed7ce4e490706b3d8b38bd538f
pointcrash <r.s.ricce@gmail.com>
2024-12-25 00:19:46 +0100

    add url for place custom order

--- traider_bot/orders/urls_api.py
+++ traider_bot/orders/urls_api.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from orders.views_api import PositionViewSet, CancelOrderView
+from orders.views_api import PositionViewSet, CancelOrderView, PlaceManualOrderView
 
 urlpatterns = [
     path('positions/', PositionViewSet.as_view({'get': 'list_by_bots'})),
     path('orders/cancel-order/<int:order_pk>/', CancelOrderView.as_view()),
-
+    path('orders/place-order/', PlaceManualOrderView.as_view()),
 ]
