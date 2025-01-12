8f136a78b9132330dde4026d1599903a27ba2363
pointcrash <r.s.ricce@gmail.com>
2025-01-12 15:15:52 +0100

    change logic getting psn_list by bots

--- traider_bot/orders/views_api.py
+++ traider_bot/orders/views_api.py
@@ -22,18 +22,22 @@ class PositionViewSet(viewsets.ModelViewSet):
     def list_by_bots(self, request):
         bots_id_list = request.query_params.getlist('bot', [])
         bots_list = BotModel.objects.filter(id__in=bots_id_list)
-        positions = []
+        positions_by_bot = dict()
 
         for bot in bots_list:
-            lp = Position.objects.filter(account=bot.account, symbol_name=bot.symbol.name, side='LONG').last()
-            sp = Position.objects.filter(account=bot.account, symbol_name=bot.symbol.name, side='SHORT').last()
-            if lp:
-                positions.append(lp)
-            if sp:
-                positions.append(sp)
+            psn_list = get_position_inform(bot)
+            positions_by_bot[bot.pk] = psn_list
 
-        serializer = self.get_serializer(positions, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        #     lp = Position.objects.filter(account=bot.account, symbol_name=bot.symbol.name, side='LONG').last()
+        #     sp = Position.objects.filter(account=bot.account, symbol_name=bot.symbol.name, side='SHORT').last()
+        #     if lp:
+        #         positions.append(lp)
+        #     if sp:
+        #         positions.append(sp)
+        #
+        # serializer = self.get_serializer(positions, many=True)
+
+        return Response(positions_by_bot, status=status.HTTP_200_OK)
 
 
 def login_test(request):
