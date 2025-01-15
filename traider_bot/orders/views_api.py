6441f99f11ab4e947a3bea329d462609761c57e2
pointcrash <r.s.ricce@gmail.com>
2025-01-15 16:43:54 +0100

    \

--- traider_bot/orders/views_api.py
+++ traider_bot/orders/views_api.py
@@ -204,11 +204,11 @@ class GetOrdersHistoryByTimeView(APIView):
             bot_id = int(body.get('bot_id'))
             bot = BotModel.objects.get(id=bot_id)
 
-            start_time = body.get('start_time')  # Unix timestamp
-            end_time = body.get('end_time')
+            start_time = int(body.get('start_time'))  # Unix timestamp
+            end_time = int(body.get('end_time'))
 
-            start_time = datetime.fromisoformat(start_time)
-            end_time = datetime.fromisoformat(end_time)
+            start_time = datetime.fromtimestamp(start_time)
+            end_time = datetime.fromtimestamp(end_time)
 
             order_history = Order.objects.filter(account=bot.account, symbol_name=bot.symbol.name, status='FILLED',
                                                  time_create__range=(start_time, end_time)).order_by('-time_update')
