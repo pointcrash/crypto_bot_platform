d50b5d7fd4b16c1f5c0590f79aeafacce775ae17
pointcrash <r.s.ricce@gmail.com>
2025-02-10 13:21:16 +0100

    \

--- traider_bot/traider_bot/adapters.py
+++ traider_bot/traider_bot/adapters.py
@@ -8,8 +8,14 @@ from django.urls import reverse
 class MyAccountAdapter(DefaultAccountAdapter):
 
     def get_email_confirmation_url(self, request, emailconfirmation):
-        lang = request.data.get("lang", "sddsg")
-        print(lang)
+        lang = "not_lang"
+
+        if request.body:
+            try:
+                data = json.loads(request.body)
+                lang = data.get("lang", lang)  # Если "lang" есть, берём его, иначе "ru"
+            except json.JSONDecodeError:
+                pass
 
         url = reverse("front_custom_confirm_email", args=[emailconfirmation.key])
         return f"{settings.FRONTEND_URL}/{lang}{url}"
