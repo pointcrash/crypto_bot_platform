6edec986b4f2f3ac1bc650a10b0d4213055a312d
pointcrash <r.s.ricce@gmail.com>
2025-02-10 12:52:33 +0100

    add 'lang' to front_custom_confirm_email

--- traider_bot/traider_bot/adapters.py
+++ traider_bot/traider_bot/adapters.py
@@ -1,3 +1,5 @@
+import json
+
 from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
 from django.urls import reverse
@@ -6,6 +8,9 @@ from django.urls import reverse
 class MyAccountAdapter(DefaultAccountAdapter):
 
     def get_email_confirmation_url(self, request, emailconfirmation):
-        lang = request.POST.get("lang", "ru")
+        # lang = request.POST.get("lang", "ru")
+        data = json.loads(request.body)
+        lang = data.get("lang", "ru")
+
         url = reverse("front_custom_confirm_email", args=[emailconfirmation.key])
         return f"{settings.FRONTEND_URL}/{lang}{url}"
