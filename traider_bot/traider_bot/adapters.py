9cfd78cd315a4b33dc071bfa4009f9c77e48600d
pointcrash <r.s.ricce@gmail.com>
2025-02-11 14:59:21 +0100

    get lang from request for email confirm message

--- traider_bot/traider_bot/adapters.py
+++ traider_bot/traider_bot/adapters.py
@@ -3,12 +3,12 @@ import json
 from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
 from django.urls import reverse
+from django.utils.translation import get_language_from_request
 
 
 class MyAccountAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
-        lang = 'ru'
-        # user_language = request.LANGUAGE_CODE
+        user_language = get_language_from_request(request)
 
         url = reverse("front_custom_confirm_email", args=[emailconfirmation.key])
-        return f"{settings.FRONTEND_URL}/{lang}{url}"
+        return f"{settings.FRONTEND_URL}/{user_language}{url}"
